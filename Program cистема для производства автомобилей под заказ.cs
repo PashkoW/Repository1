695c87fd5ba264e89f6ef9f4823dd7d30613b976
PashkoW <90702715+PashkoW@users.noreply.github.com>
2021-09-14 17:33:03 +0300

    Add files via upload

---
+++ Program cистема для производства автомобилей под заказ.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // cистема для производства автомобилей под заказ
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
